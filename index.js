5fc44e65a9a4bcb878eee37b1fcdc5d408d33c99
Cynthia Nezames <cnezames@gmail.com>
2018-08-28 01:48:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector) {
+  const first = document.querySelector(selector)
+  return first
+}
 
+function nestedTarget() {
+  const targetnested = document.getElementById('nested').querySelector('.target')
+  return targetnested
+}
+
+function increaseRankBy(n) {
+  const ranked = document.querySelectorAll('.ranked-list')
+  for (let i = 0; i < ranked.length; i++) {
+    ranked[i].innerHTML = (i + n).toString()
+  }
+  return ranked
+}
+
+function deepestChild() {
+  const child = document.getElementById('grand-node')
+  child.querySelector('div')
+  return child
+}
